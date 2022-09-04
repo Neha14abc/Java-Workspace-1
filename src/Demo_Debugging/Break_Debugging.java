ef0b22056ffd9243a2e1189afab18ca4e2d8f68d
Neha14abc <kusum14032000@gmail.com>
2022-09-04 19:25:32 +0530

    variableManipulation 2

---
+++ src/Demo_Debugging/Break_Debugging.java
@@ -0,0 +1,16 @@
+package Demo_Debugging;
+
+public class Break_Debugging {
+    public static void main(String[] args) {
+        int day =2;
+        switch (day) {
+            case 1:
+                System.out.println("First day of a Week: Monday");
+                break;
+            case 2:
+                System.out.println("Second day of a Week: Tuesday");
+                break;
+        }
+    }
+}
+
---
+++ src/Demo_Debugging/Continue_Debugging.java
@@ -0,0 +1,13 @@
+package Demo_Debugging;
+
+public class Continue_Debugging {
+        public static void main(String[] args) {
+            int number = 2375;
+            if (number % 2 == 0) {
+                System.out.println("They are EVEN ..." + number);
+            }
+            if (number % 2 != 0) {
+                System.out.println("They are ODD ..." + number);
+            }
+        }
+}
